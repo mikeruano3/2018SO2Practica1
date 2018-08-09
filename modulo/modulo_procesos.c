684738ba2d6e4eff9a8500e591dcb42f026ddade
mikeruano3 <miguelruano35@gmail.com>
2018-08-08 23:13:28 -0600

    primer arreglo

--- modulo/modulo_procesos.c
+++ modulo/modulo_procesos.c
@@ -14,12 +14,12 @@
 
         for_each_process(task){
 		  struct list_head *list;
-		  seq_printf(m,"{\"NAME\":\"%s\",\"USER\":\"%d\",\"PID\":\"%d\",\"CPU\":\"%d\",\"RAM\":\"%d\",\"STATUS\":",
+		  seq_printf(m,"{\"NAME\":\"%s\",\"USER\":\"%d\",\"PID\":\"%d\",\"CPU\":\"%d\",\"RAM\":\"%lu\",\"STATUS\":",
                   task->comm,
                   task->cred->uid.val,
 		          task->pid,
                   task->utime.val,
-                  task->mm->total_vm.val);
+                  task->mm->total_vm);
 		if(task->state == 0){
 			seq_printf(m,"\"RUNNING\"}");
 		}
