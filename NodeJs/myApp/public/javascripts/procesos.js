76f2fe54758bd6d71af87ed6a062533b247396bf
mikeruano3 <miguelruano35@gmail.com>
2018-08-09 00:13:59 -0600

    finalizar proceso

--- NodeJs/myApp/public/javascripts/procesos.js
+++ NodeJs/myApp/public/javascripts/procesos.js
@@ -37,16 +37,21 @@ $(function() {
                 dataType: 'json',
                 success : function(data){
                     if(data.tipo == "NORMAL"){
-                        var i, html, iteml;
+                        var i, jk, html, iteml, memxproc;
                         html = "";
                         document.getElementById("listado").innerHTML = "";
+                        var total = 0;
+                        for (jk = 0; jk < data.arreglo.length; jk++){
+                            total = total + data.arreglo[i].mem;
+                        }
                         for (i = 0; i < data.arreglo.length; i++) {
+                            memxproc = data.arreglo[i].mem/total;
                             html += "<tr>";
                             html  += '<td>' + data.arreglo[i].nombre + '</td>';
                             html  += '<td>' + data.arreglo[i].user + '</td>';
                             html  += '<td>' + data.arreglo[i].pid + '</td>';
                             html  += '<td>' + data.arreglo[i].status + '</td>';
-                            html  += '<td>' + data.arreglo[i].mem + '</td>';
+                            html  += '<td>' + memxproc + '</td>';
                             html  += '<td>' + data.arreglo[i].cpu + '</td>';
                             html  += '<td><a class="btn btn-danger" href="/proceso/matar/'+ data.arreglo[i].pid+'">Matar</a></td>';
                             html += '</tr>';
